94b2c4cec75b32c637efaafe63e3b3e7772fe3af
EliasMartiniuc <eliasmartiniuc123@gmail.com>
2026-08-13 14:37:12 +0300

    Fix missing Form import from fastapi

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from typing import Dict, List, Optional
 from datetime import datetime
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, Request, UploadFile, File
+from fastapi import FastAPI, HTTPException, Request, UploadFile, File, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
